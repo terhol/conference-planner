f2869471f6f679b2ddfb1e6407cf9820699914a6
Holm <tereza.holm@sogeti.com>
2020-03-13 14:24:05 +0100

    Controller for Registration set up

--- src/main/java/se/terhol/conference/controller/GreetingController.java
+++ src/main/java/se/terhol/conference/controller/GreetingController.java
@@ -1,4 +1,17 @@
 package se.terhol.conference.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
 public class GreetingController {
+
+    @GetMapping("greeting")
+    public String returnGreeting(Map<String, Object> model){
+        model.put("message", "Hello world");
+        return "greeting";
+
+    }
 }
---
+++ src/main/java/se/terhol/conference/model/Registration.java
@@ -0,0 +1,9 @@
+package se.terhol.conference.model;
+
+import lombok.Data;
+
+@Data
+public class Registration {
+
+    private String name;
+}
